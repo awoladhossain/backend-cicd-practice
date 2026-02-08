1826f41b2b9927c1957b9ae772709b7fc24b0b35
awoladhossain <awoladh04@gmail.com>
2026-02-09 00:46:14 +0600

    updated

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
@@ -10,8 +9,12 @@ app.get("/", (req, res) => {
 
 const add = (a, b) => a + b;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// পরিবর্তন এখানে:
+// যদি এনভায়রনমেন্ট 'test' না হয়, তবেই কেবল সার্ভার চালু হবে।
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
 export { add, app };
