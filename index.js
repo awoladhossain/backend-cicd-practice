1bf9685a3043c828f53d8d57801b2ebd337ec831
awoladhossain <awoladh04@gmail.com>
2026-02-09 23:23:46 +0600

    vercel.json file uploaded

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, CI/CD is working!");
 });
 
+app.get("/api/status", (req, res) => {
+  res.status(200).json({ status: "running" });
+});
+
 const add = (a, b) => a + b;
 
-if (process.env.NODE_ENV !== "test") {
+
+if (process.env.NODE_ENV !== "test" && !process.env.VERCEL) {
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
+
+export default app;
 export { add, app };
