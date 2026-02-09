73a2de51e6b12f05ae7719d7d1495c308115274d
awoladhossain <awoladh04@gmail.com>
2026-02-09 23:02:14 +0600

    added vercel deployment automation

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ app.get("/", (req, res) => {
 
 const add = (a, b) => a + b;
 
-// পরিবর্তন এখানে:
-// যদি এনভায়রনমেন্ট 'test' না হয়, তবেই কেবল সার্ভার চালু হবে।
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
